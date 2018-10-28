d0c2b7e08dd6a0b4c1e5a7a19c5605c481742b90
Nianor <Nianor@users.noreply.github.com>
2018-10-28 00:16:24 -0500

    Delete Config.java

--- common/config/Config.java
+++
@@ -1,7 +0,0 @@
-package com.nianor.tinkersarsenal.common.config;
-
-import slimeknights.mantle.pulsar.config.ForgeCFG;
-
-public class Config {
-    //public static final ForgeCFG IDKWhatImDoing = new ForgeCFG("")
-}
